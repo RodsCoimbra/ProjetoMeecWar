43db224783781acf21c4ba9697a9103737649b13
Rodrigo Coimbra <80896966+RodsCoimbra@users.noreply.github.com>
2021-03-22 20:49:05 +0000

    Rename main.c to layer2.c

---
+++ layer2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int linha2 = 3, coluna2 = 2, i, j, resj, resi, camada2[16][25];
+    camada2 [linha2][coluna2] = 2;
+    j = resj;
+    i = resi;
+    for(i = -1; i <= 1; i++){
+            for(j=-1; j <= 1; j++){
+                if(camada2[linha2+i][coluna2+j] == 2){}
+                else{
+                    camada2[linha2+i][coluna2+j] = 1;
+                }
+        }
+    }
+}
